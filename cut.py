55e4a07bb1b6cf3df92628418f5ce5e300d097ef
Vyacheslav Fedorov <vyacheslav.fedorov@sibers.com>
2022-03-22 15:42:33 +0700

    Add autoscale cut script

--- cut.py
+++ cut.py
@@ -1,14 +1,19 @@
 import cv2 as cv
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("logo", type=str, help="the logo path")
-parser.add_argument("image", type=str, help="the image path")
-parser.add_argument("target", type=str, help="the crop image path")
-args = parser.parse_args()
+ap = argparse.ArgumentParser()
+ap.add_argument("-l", "--logo", type=str, required=True,
+                help="path to logo image")
+ap.add_argument("-i", "--image", type=str, required=True,
+                help="path to input image where we'll apply template matching")
+ap.add_argument("-t", "--target", type=str, required=True,
+                help="path to logo image")
+args = vars(ap.parse_args())
 
-img_rgb, template_rgb = cv.imread(args.image), cv.imread(args.logo)
-img_gray, template_gray = cv.cvtColor(img_rgb, 6), cv.cvtColor(template_rgb, 6)
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(cv.matchTemplate(img_gray, template_gray, 0))
-w, h = img_gray.shape[::-1]
-cv.imwrite(args.target, img_rgb[0:max_loc[1], 0:w])
+print("[INFO] loading images...")
+img_rgb, template_rgb = cv.imread(args["image"]), cv.imread(args["logo"])
+img_gray, template_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY), cv.cvtColor(template_rgb, cv.COLOR_BGR2GRAY)
+
+print("[INFO] performing template matching...")
+min_val, max_val, min_loc, max_loc = cv.minMaxLoc(cv.matchTemplate(img_gray, template_gray, cv.TM_SQDIFF))
+cv.imwrite(args["target"], img_rgb[0:max_loc[1], 0:img_gray.shape[::-1][0]])
--- cut_research.py
+++ cut_research.py
@@ -1,30 +1,77 @@
-import cv2 as cv
-import numpy as np
-from matplotlib import pyplot as plt
+# USAGE
+# python match.py --template cod_logo.png --images images
 
-img = cv.imread('image.jpg',0)
-img2 = img.copy()
-template = cv.imread('logo.png',0)
-w, h = template.shape[::-1]
-# All the 6 methods for comparison in a list
-methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-            'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
-for meth in methods:
-    img = img2.copy()
-    method = eval(meth)
-    # Apply template Matching
-    res = cv.matchTemplate(img,template,method)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv.rectangle(img,top_left, bottom_right, 255, 2)
-    plt.subplot(121),plt.imshow(res,cmap = 'gray')
-    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(img,cmap = 'gray')
-    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(meth)
-    plt.show()
+# import the necessary packages
+
+import numpy as np
+import argparse
+import imutils
+import glob
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-t", "--template", required=True, help="Path to template image")
+ap.add_argument("-i", "--images", required=True,
+                help="Path to images where template will be matched")
+ap.add_argument("-v", "--visualize",
+                help="Flag indicating whether or not to visualize each iteration")
+args = vars(ap.parse_args())
+
+# load the image image, convert it to grayscale, and detect edges
+template = cv2.imread(args["template"])
+template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+template = cv2.Canny(template, 50, 200)
+(tH, tW) = template.shape[:2]
+cv2.imshow("Template", template)
+
+# loop over the images to find the template in
+for imagePath in glob.glob(args["images"] + "/*.jpg"):
+    # load the image, convert it to grayscale, and initialize the
+    # bookkeeping variable to keep track of the matched region
+    image = cv2.imread(imagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    found = None
+
+    # loop over the scales of the image
+    for scale in np.linspace(0.1, 5, 50)[::-1]:
+        # resize the image according to the scale, and keep track
+        # of the ratio of the resizing
+        resized = imutils.resize(gray, width=int(gray.shape[1] * scale))
+        r = gray.shape[1] / float(resized.shape[1])
+
+        # if the resized image is smaller than the template, then break
+        # from the loop
+        if resized.shape[0] < tH or resized.shape[1] < tW:
+            break
+
+        # detect edges in the resized, grayscale image and apply template
+        # matching to find the template in the image
+        edged = cv2.Canny(resized, 50, 200)
+        result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
+        (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+
+        # check to see if the iteration should be visualized
+        if args.get("visualize", False):
+            # draw a bounding box around the detected region
+            clone = np.dstack([edged, edged, edged])
+            cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
+                          (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
+            cv2.imshow("Visualize", clone)
+            cv2.waitKey(0)
+
+        # if we have found a new maximum correlation value, then ipdate
+        # the bookkeeping variable
+        if found is None or maxVal > found[0]:
+            found = (maxVal, maxLoc, r)
+
+    # unpack the bookkeeping varaible and compute the (x, y) coordinates
+    # of the bounding box based on the resized ratio
+    (_, maxLoc, r) = found
+    (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+    (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+
+    # draw a bounding box around the detected result and display the image
+    cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
