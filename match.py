29a97e7324176634d25011ba05eda29779dc23b0
Vyacheslav Fedorov <vyacheslav.fedorov@sibers.com>
2022-03-22 17:05:31 +0700

    update

--- match.py
+++ match.py
@@ -1,29 +1,28 @@
 # USAGE
-# python match.py --template cod_logo.png --images images
+# python match.py -i image.jpg -o crop.jpg -t logo.png -v y
 
 # import the necessary packages
-
 import numpy as np
 import argparse
 import imutils
-import glob
 import cv2
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True,
+                help="Path to image where template will be matched")
+ap.add_argument("-o", "--output",
+                help="Path to result image where template cut")
 ap.add_argument("-t", "--template", required=True, help="Path to template image")
-ap.add_argument("-i", "--images", required=True,
-                help="Path to images where template will be matched")
 ap.add_argument("-min", "--min_scale", default=0.1,
                 help="Min scale image in the loop over the scales of the image")
-ap.add_argument("-max", "--max_scale", default=2,
+ap.add_argument("-max", "--max_scale", default=3,
                 help="Max scale image in the loop over the scales of the image")
-ap.add_argument("-n", "--number", default=50,
+ap.add_argument("-n", "--number", default=100,
                 help="Number of scales image in the loop over the scales of the image")
 ap.add_argument("-v", "--visualize",
                 help="Flag indicating whether or not to visualize each iteration")
-# ap.add_argument("-c", "--cut",
-#                 help="Flag indicating whether or not to cut each template in the each image")
+
 args = vars(ap.parse_args())
 
 # load the image image, convert it to grayscale, and detect edges
@@ -32,58 +31,59 @@ template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 template = cv2.Canny(template, 50, 200)
 (tH, tW) = template.shape[:2]
 
-# loop over the images to find the template in
-for imagePath in glob.glob(args["images"] + "/*.jpg"):
-    # load the image, convert it to grayscale, and initialize the
-    # bookkeeping variable to keep track of the matched region
-    image = cv2.imread(imagePath)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    found = None
 
-    # loop over the scales of the image
-    for scale in np.linspace(args["min_scale"], args["max_scale"], args["number"])[::-1]:
-        # resize the image according to the scale, and keep track
-        # of the ratio of the resizing
-        resized = imutils.resize(gray, width=int(gray.shape[1] * scale))
-        r = gray.shape[1] / float(resized.shape[1])
+# load the image, convert it to grayscale, and initialize the
+# bookkeeping variable to keep track of the matched region
+image = cv2.imread(args["input"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+found = None
 
-        # if the resized image is smaller than the template, then break
-        # from the loop
-        if resized.shape[0] < tH or resized.shape[1] < tW:
-            break
+# loop over the scales of the image
+for scale in np.linspace(args["min_scale"], args["max_scale"], args["number"])[::-1]:
+    # resize the image according to the scale, and keep track
+    # of the ratio of the resizing
+    resized = imutils.resize(gray, width=int(gray.shape[1] * scale))
+    r = gray.shape[1] / float(resized.shape[1])
 
-        # detect edges in the resized, grayscale image and apply template
-        # matching to find the template in the image
-        edged = cv2.Canny(resized, 50, 200)
-        result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
-        (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+    # if the resized image is smaller than the template, then break
+    # from the loop
+    if resized.shape[0] < tH or resized.shape[1] < tW:
+        break
 
-        # check to see if the iteration should be visualized
-        if args.get("visualize", False):
-            # draw a bounding box around the detected region
-            clone = np.dstack([edged, edged, edged])
-            cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
-                          (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
-            cv2.imshow("Template", template)
-            cv2.imshow("Visualize", clone)
-            cv2.waitKey(0)
+    # detect edges in the resized, grayscale image and apply template
+    # matching to find the template in the image
+    edged = cv2.Canny(resized, 50, 200)
+    result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
+    (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
 
-        # if we have found a new maximum correlation value, then ipdate
-        # the bookkeeping variable
-        if found is None or maxVal > found[0]:
-            found = (maxVal, maxLoc, r)
+    # check to see if the iteration should be visualized
+    if args.get("visualize", False):
+        # draw a bounding box around the detected region
+        clone = np.dstack([edged, edged, edged])
+        cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
+                      (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
+        cv2.imshow("Template", template)
+        cv2.imshow("Visualize", clone)
+        cv2.waitKey(0)
 
-    # unpack the bookkeeping varaible and compute the (x, y) coordinates
-    # of the bounding box based on the resized ratio
-    (_, maxLoc, r) = found
-    (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
-    (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+    # if we have found a new maximum correlation value, then ipdate
+    # the bookkeeping variable
+    if found is None or maxVal > found[0]:
+        found = (maxVal, maxLoc, r)
 
-    # draw a bounding box around the detected result and display the image
+# unpack the bookkeeping varaible and compute the (x, y) coordinates
+# of the bounding box based on the resized ratio
+(_, maxLoc, r) = found
+(startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+(endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+
+# draw a bounding box around the detected result and display the image
+if args.get("visualize", False):
     cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
     cv2.imshow("Image", image)
     cv2.waitKey(0)
 
-    # if args.get("cut", False):
-    #     cv2.imwrite("crop/" + imagePath, image[0:startY, 0:gray.shape[::-1][0]])
+# cut a line with a template at the bottom of an image
+if args.get("output", False):
+    cv2.imwrite(args["output"], image[0:startY, 0:gray.shape[::-1][0]])
 
